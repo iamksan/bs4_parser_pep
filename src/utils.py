e109f89ee0774ee2ae6352860a7f2e3326bfd864
Никита Остапенко <nikitaostapenko0968@gmail.com>
2023-07-08 12:51:16 +0500

    first

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import logging
+
+import bs4
+from requests import RequestException
+
+from exceptions import ParserFindTagException
+
+
+def get_response(session, url):
+    try:
+        response = session.get(url)
+        response.encoding = 'utf-8'
+        return response
+    except RequestException:
+        logging.exception(
+            f'Возникла ошибка при загрузке страницы {url}',
+            stack_info=True
+        )
+
+
+def find_tag(soup, tag=None, attrs=None, **kwargs) -> bs4.element.Tag:
+    searched_tag = soup.find(tag, attrs=(attrs or {}), **kwargs)
+    if searched_tag is None:
+        error_msg = f'Не найден тег {tag} {attrs}'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_tag
